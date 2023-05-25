6ae320a1ad559932f1efed55ea7fda13e228e697
Gitta <mail@gvanderpol.nl>
2023-05-25 12:13:52 +0200

    Add the author controller and  model tests

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,50 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to authors_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @author.destroy
+    redirect_to authors_path, status: :see_other
+  end
+
+  private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
 end
--- test/controllers/authors_controller_test.rb
+++ test/controllers/authors_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 class AuthorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get authors_path
+    assert_select "h1", "Authors"
+    assert_select "table > tbody > tr", count: Author.count
+    assert_select "table > tbody > tr:nth-child(1) td", Author.first.name
+  end
+
+  test "should create new author" do
+    new_author_name = "Some name"
+
+    assert_difference("Author.count", +1) do
+      post authors_path, params: { author: { name: new_author_name } }
+    end
+    assert_redirected_to authors_path
+    new_author = Author.last
+    assert_equal new_author_name, new_author.name
+  end
+
+  test "should return error message when creating an incorrect author" do
+    assert_no_difference("Author.count") do
+      post authors_path, params: { author: { name: "" } }
+      assert_select "div.alert.alert-danger", "The form contains 1 error."
+      assert_select "ul > li", "Name can't be blank"
+    end
+  end
+
+  test "should show author" do
+    author = authors(:first)
+    get author_path(author)
+    assert_select "h1", "Jake Olson"
+  end
+
+  test "should edit author" do
+    author = authors(:first)
+    get author_path(author)
+    assert_select "h1", "Jake Olson"
+    patch author_path, params: { author: { name: "Jane Olson" } }
+    assert_redirected_to author_path(author)
+    follow_redirect!
+    assert_select "h1", "Jane Olson"
+  end
+
+  test "should destroy author" do
+    author = authors(:first)
+
+    assert_difference("Author.count", -1) do
+      delete author_path(author)
+    end
+
+    assert_redirected_to authors_path
+  end
 end
